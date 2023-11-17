9990a2326f84a27361aca91f4e44e76c9f96b227
Matheus Batista <mbatista.sarti@gmail.com>
2023-11-16 21:29:13 -0300

    feat

--- problems/problem_03.py
+++ problems/problem_03.py
@@ -3,18 +3,9 @@ from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
 
-        my_list = []
-        my_list2 = []
+        greatest_value = max(candies)
 
-        for index, kid in enumerate(candies):
-            my_list.append(True)
-            for index2, others_kid in enumerate(candies):
-                if index != index2:
-                    if kid + extraCandies < others_kid:
-                        my_list[index] = False
-                        break
-                    
-        return my_list
+        return print([x + extraCandies >= greatest_value for x in candies])
 
 candies = [2,3,5,1,3] 
 extraCandies = 3
